cb0ed508a103810a90f46eacdb964cfed36cdc41
Rezende <rezendedev@gmail.com>
2023-08-23 18:45:25 -0300

    First Commit

---
+++ Brushier.lua
@@ -0,0 +1,123 @@
+if app.apiVersion < 22 then
+    return app.alert("This script requires Aseprite v1.3-rc2")
+end
+
+
+local dialog = Dialog{
+    title="Brushier",
+    notitlebar=false,
+   
+}
+
+local previousSize = app.brush.size;
+local brushType = app.brush.type;
+
+local plt = app.sprite.palettes[1];
+local clrs = {}
+
+local current = 0
+
+while current < #plt do
+    clrs[current] = plt:getColor(current)
+    current = current + 1
+end
+
+local function changeBrushType()
+    local nBrush = Brush{
+        size = previousSize,
+        type = brushType
+    }
+    app.brush = nBrush
+end
+
+dialog
+:button{ 
+    id="brushTypeS",
+    text="Square",
+    onclick= function(ev)
+        brushType = BrushType.SQUARE
+        changeBrushType()
+
+    end
+}
+:button{ 
+    id="brushTypeC",
+    text="Circle",
+    onclick= function(ev)
+        brushType = BrushType.CIRCLE
+        changeBrushType()
+    end
+}
+:separator{}
+:slider{
+    id = "brushSize",
+    min=1,
+    max=64,
+    value=app.brush.size,
+    onrelease = function(ev)
+       local data = dialog.data.brushSize
+       local nBrush = Brush{
+        type = brushType,
+        size = data
+        }
+        app.brush = nBrush
+
+        local i = previousSize - data;
+        local j = 0;
+        if i < 0 then
+            i = i * -1;
+            while j < i do
+                app.command.ChangeBrush{
+                    change = "increment-size"
+                }
+                j = j + 1  
+            end
+        else 
+            while j < i do
+                app.command.ChangeBrush{
+                    change = "decrement-size"
+                }
+                j = j + 1  
+            end
+        end
+
+        previousSize = data;
+    end
+}
+:separator{}
+
+current = 0;
+local shades = {table.unpack(clrs, current, 4)}
+
+while #shades > 0 do
+    dialog:shades{ 
+        id="pallete",
+        label="",
+        mode="pick",
+        colors=shades,
+        onclick=function(ev)
+            app.fgColor = ev.color
+        end
+    }
+    current = current + 6;
+    shades = {table.unpack(clrs, current, current + 4)}
+    
+    if shades == nil then
+        break;
+    end
+
+end
+
+dialog:separator{}
+:button{ 
+    id="close",
+    text="Close (Esc)",
+    onclick= function(ev)
+        dialog:close()
+    end
+}
+
+local bounds = dialog.bounds
+dialog.bounds = Rectangle(app.editor.mousePos.x - (bounds.width / 2) - 6, app.editor.mousePos.y - (bounds.height / 2), bounds.width, bounds.height)
+
+dialog:show()
---
+++ README.md
@@ -0,0 +1,47 @@
+# Brushier
+
+![Brushier](./pics/Brushier_Example.png)
+
+Brushier is a plugin for Aseprite that allows you to have a quick brush menu right at your mouse position.
+
+## Disclaimer
+
+This script requires Aseprite v1.3-rc2 or greater.
+
+## Installation
+
+![Tutorial - Part 1](./pics/Tuto_01.png)
+
+-   Click on File.
+
+![Tutorial - Part 2](./pics/Tuto_02.png)
+
+-   Open Scripts Folder and paste Brushier.lua file inside of it.
+
+![Tutorial - Part 3](./pics/Tuto_03.png)
+
+-   Rescan Scripts Folder (F5).
+
+![Tutorial - Part 4](./pics/Tuto_04.png)
+
+-   Go to Edit and select Keyboard Shortcuts.
+
+![Tutorial - Part 5](./pics/Tuto_05.png)
+
+-   Search for "Script" and click on "Add" (Key) at "Run Script: Brushier.lua" row.
+
+![Tutorial - Part 6](./pics/Tuto_06.png)
+
+-   I recommend you to use "Shift + A" but you can use any unused shortcut that you prefer.
+
+-   Done! Now press Shift + A and Brushier will open.
+
+## Examples of Brushier
+
+![Example  - 1](./pics/B_Example_One_row.png)
+
+![Example  - 2](./pics/B_Example_Three_rows.png)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
